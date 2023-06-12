477f0715b99dc2677498b3240eb3db554a908406
JocasChitende31 <84420296+JocasChitende31@users.noreply.github.com>
2023-06-12 15:49:54 +0100

    first-class: position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,27 @@
 package application;
 
+import java.util.Scanner;
+
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Posicion (row): ");
+		int row = sc.nextInt();
+		System.out.print("Posicion (column): ");
+		int column = sc.nextInt();
+		Position position = new Position(row,column);
+		
+		System.out.println("Posiction: " + position);
+		
+		
+		sc.close();
 	}
 
 }
