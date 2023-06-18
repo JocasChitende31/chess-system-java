444fe51948f4ed98cef2139a893876c081af6c2d
JocasChitende31 <84420296+JocasChitende31@users.noreply.github.com>
2023-06-18 15:05:53 +0100

    Boarding exception and defensive programming

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -29,8 +29,8 @@ public class ChessMatch {
 
 	public void initialSetup() {
 
-		board.placePiece(new Rook(board, Color.BLACK), new Position(2,2));
-		board.placePiece(new King(board, Color.WHITE), new Position(3,2));
-		board.placePiece(new Queen(board, Color.WHITE), new Position(7,7));
+		board.placePiece(new King(board, Color.WHITE), new Position(2,1));
+		board.placePiece(new Rook(board, Color.BLACK), new Position(0,4));
+		board.placePiece(new Queen(board, Color.WHITE), new Position(7,4));
 	}
 }
